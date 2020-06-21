c2d3ade783fc882363f55810ac4a7bcc3aa2e8b9
patrykkoziel <patrykkoziel99@o2.pl>
2020-06-21 15:24:05 +0200

    Task 1

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,20 @@
 /* Here goes your JS code */
+
+const popup = document.getElementsByClassName("popup");
+const showPopupBtn = document.getElementById("show-popup-form");
+const openPopup = () => {
+  popup[0].style.visibility = "visible";
+  showPopupBtn.style.visibility = "hidden";
+};
+const closePopup = () => {
+  popup[0].style.visibility = "hidden";
+  showPopupBtn.style.visibility = "visible";
+};
+
+document.getElementById("close-popup-form").addEventListener("click", () => {
+  closePopup();
+});
+
+document.getElementById("show-popup-form").addEventListener("click", () => {
+  openPopup();
+});
